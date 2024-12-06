597e010058d2eb6faafac52ba392b9b54dcbc3ec
miraskazik <kazik.m@email.cz>
2024-12-07 00:20:08 +0100

    001 - init aplikace

---
+++ src/test/java/cz/rb/bcreservation/BcReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.rb.bcreservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BcReservationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
